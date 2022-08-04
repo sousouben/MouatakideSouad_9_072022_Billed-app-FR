21455aaae6163c1007fd7b06c85f86207e611ab2
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2022-08-04 11:03:05 +0200

    correction de [Bug report] - Bills

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -31,7 +31,7 @@ describe("Given I am connected as an employee", () => {//Étant donné que je su
     test("Then bills should be ordered from earliest to latest", () => {//Ensuite, les factures doivent être commandées du plus ancien au plus tard
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const antiChrono = (a, b) => (a-b)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
