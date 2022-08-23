3a3b05f8be2138ce32aaf9ae2ddb8d82f55690d9
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2022-08-23 09:49:38 +0200

    AJOUT de expect dans test bills

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,9 +2,9 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
+import { screen, waitFor } from "@testing-library/dom";
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 
@@ -12,30 +12,44 @@ import router from "../app/Router.js";
 
 //test sur la page employée des factures
 
-describe("Given I am connected as an employee", () => {//Étant donné que je suis connecté en tant qu'employé
-  describe("When I am on Bills Page", () => {//Quand je suis sur la page Bills(factures)
-    test("Then bill icon in vertical layout should be highlighted", async () => {//Ensuite, l'icône de la facture dans la disposition verticale doit être mise en surbrillance
+describe("Given I am connected as an employee", () => {
+  //Étant donné que je suis connecté en tant qu'employé
+  describe("When I am on Bills Page", () => {
+    //Quand je suis sur la page Bills(factures)
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+      //Ensuite, l'icône de la facture dans la disposition verticale doit être mise en surbrillance
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId("icon-window"));
+      const windowIcon = screen.getByTestId("icon-window");
       //to-do write expect expression
-      expect(windowIcon.className).toContain("active-icon")
-    })
-    test("Then bills should be ordered from earliest to latest", () => {//Ensuite, les factures doivent être commandées du plus ancien au plus tard
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => (a-b)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
-})
+      expect(windowIcon.className).toContain("active-icon");
+    });
+    
+    test("Then bills should be ordered from earliest to latest", () => {
+      //Ensuite, les factures doivent être commandées du plus ancien au plus tard
+      document.body.innerHTML = BillsUI({ data: bills });
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => a - b;
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+  });
+});
